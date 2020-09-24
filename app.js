21d943aed3faae0f05fcdea2dc31c989609c7086
Alejandra Cano Sosa <alejandra.cano@itexico.com>
2020-09-23 22:03:13 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+/*let persona1={
+    nombre:'Diana',
+    apellido:'Perez',
+    edad: 21,
+    nacionalidad: 'mexicana'
+}
+console.log  (persona1);
+console.log(persona1.nombre);
+
+let gato={
+    nombre:'angustino',
+    color: 'atrigado',
+    raza: 'mestizo',
+    sexo: 'macho',
+    ladrar(){
+        console.log ('miau, miau');
+    },
+    caminar(){
+        console.log('caminando');
+    },
+    bolaPelo(){
+        this.color='mucho pelo';
+    }
+
+}
+console.log(gato);
+gato.ladrar();
+gato.bolaPelo();
+console.log (gato);
+gato.patas= 4;
+gato.salud='saludable';
+*/
+
